6ce6992b332ff90e70eda277731917fb4f50294e
Ruchika Sehgal <88452067+Ruchika-Sehgal@users.noreply.github.com>
2023-02-02 21:24:58 +0530

    Add files via upload

---
+++ src/context/TrendingContext.js
@@ -0,0 +1,43 @@
+import { createContext, useLayoutEffect, useState } from "react";
+
+// create context object
+export const TrendingContext = createContext({});
+
+// create the provider component
+export const TrendingProvider = ({ children }) => {
+  const [trendData, setTrendData] = useState();
+
+  const getTrendData = async () => {
+    try {
+      const data = await fetch(
+        `https://api.blockcypher.com/v1/eth/main`
+      )
+        .then((res) => { return res.json() })
+        .then((json) => { return json });
+
+      console.log(data);
+      setTrendData(data.coins);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const resetTrendingResult = () => {
+    getTrendData();
+  };
+
+  useLayoutEffect(() => {
+    getTrendData();
+  }, []);
+
+  return (
+    <TrendingContext.Provider
+      value={{
+        trendData,
+        resetTrendingResult,
+      }}
+    >
+      {children}
+    </TrendingContext.Provider>
+  );
+};
